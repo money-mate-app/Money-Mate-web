9aa957201b7d290bb142683c45b995df72efc785
vinay2812 <vinaysarda2812@gmail.com>
2023-07-26 03:02:59 +0530

    update env import

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,9 +1,21 @@
-export const {
-    NEXT_PUBLIC_FACEBOOK_APP_ID = "",
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID = "",
-    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET = "",
-    NEXT_PUBLIC_MICROSOFT_CLIENT_ID = "",
-    NEXT_PUBLIC_SERVER_URL = "",
-} = process.env;
+import { z } from "zod";
 export const SERVER_MODE = typeof window === undefined;
 export const __DEV__ = process.env.NODE_ENV;
+
+export const env = {
+    NEXT_PUBLIC_FACEBOOK_APP_ID: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID!,
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!,
+    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET:
+        process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET!,
+    NEXT_PUBLIC_MICROSOFT_CLIENT_ID:
+        process.env.NEXT_PUBLIC_MICROSOFT_CLIENT_ID!,
+    NEXT_PUBLIC_SERVER_URL: process.env.NEXT_PUBLIC_SERVER_URL!,
+};
+
+z.object({
+    NEXT_PUBLIC_SERVER_URL: z.string(),
+    NEXT_PUBLIC_FACEBOOK_APP_ID: z.string(),
+    NEXT_PUBLIC_GOOGLE_CLIENT_ID: z.string(),
+    NEXT_PUBLIC_GOOGLE_CLIENT_SECRET: z.string(),
+    NEXT_PUBLIC_MICROSOFT_CLIENT_ID: z.string(),
+}).parse(env);
--- src/providers/SocketProvider.tsx
+++ src/providers/SocketProvider.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { useEffect, type FC, type ReactNode, useMemo } from "react";
 import { io } from "socket.io-client";
-import { NEXT_PUBLIC_SERVER_URL, __DEV__ } from "../lib/constants";
+import { env, __DEV__ } from "../lib/constants";
 interface SocketProviderProps {
     children: ReactNode;
 }
@@ -8,7 +9,7 @@ interface SocketProviderProps {
 const SocketProvider: FC<SocketProviderProps> = ({ children }) => {
     const socket = useMemo(
         () =>
-            io(NEXT_PUBLIC_SERVER_URL, {
+            io(env.NEXT_PUBLIC_SERVER_URL, {
                 retries: 3,
                 reconnectionAttempts: 3,
                 reconnectionDelay: 1000,
@@ -20,6 +21,9 @@ const SocketProvider: FC<SocketProviderProps> = ({ children }) => {
         socket.on("connect", () => {
             __DEV__ && console.log("Server socket connected");
         });
+        return () => {
+            socket.disconnect();
+        };
     }, [socket]);
 
     return children;
