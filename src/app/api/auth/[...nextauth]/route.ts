5d9fb44066d020d83678d7e0298e1e8ab1a2c48d
Zaid <zaidodam@gmai.com>
2023-08-04 01:34:42 +0530

    fix: zod env

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,21 +2,21 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
 import GithubProvider from "next-auth/providers/github";
+import { env } from "@/lib/constants";
 
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env
-                .NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
+            clientId: env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+            clientSecret: env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
         }),
         FacebookProvider({
-            clientId: process.env.NEXT_PUBLIC_FACEBOOK_APP_ID as string,
-            clientSecret: process.env.NEXT_PUBLIC_FACEBOOK_APP_SECRET as string,
+            clientId: env.NEXT_PUBLIC_FACEBOOK_APP_ID as string,
+            clientSecret: env.NEXT_PUBLIC_FACEBOOK_APP_SECRET as string,
         }),
         GithubProvider({
-            clientId: process.env.NEXT_PUBLIC_GITHUB_APP_ID as string,
-            clientSecret: process.env.NEXT_PUBLIC_GITHUB_APP_SECRET as string,
+            clientId: env.NEXT_PUBLIC_GITHUB_APP_ID as string,
+            clientSecret: env.NEXT_PUBLIC_GITHUB_APP_SECRET as string,
         }),
     ],
     callbacks: {
